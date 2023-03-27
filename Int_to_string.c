05fd506f95b5c12ff4b52f2352a152c88e853378
petboa <112291489+petboa@users.noreply.github.com>
2023-03-27 20:04:44 +0000

    c

--- Int_to_string.c
+++ Int_to_string.c
@@ -5,7 +5,7 @@ char *get_int(int num);
 unsigned int _abs(int);
 int get_numbase_len(unsigned int num, unsigned int base);
 void fill_numbase_buff(unsigned int num, unsigned int base,
-               char *buff, int buff_size);
+char *buff, int buff_size);
 
 
 /**
@@ -16,26 +16,26 @@ void fill_numbase_buff(unsigned int num, unsigned int base,
  */
 char *get_int(int num)
 {
-    unsigned int temp;
-    int length = 0;
-    long num_l = 0;
-    char *ret;
+unsigned int temp;
+int length = 0;
+long num_l = 0;
+char *ret;
 
 
     temp = _abs(num);
-    length = get_numbase_len(temp, 10);
+  length = get_numbase_len(temp, 10);
 
 
     if (num < 0 || num_l < 0)
-        length++; /* negative sign */
+  length++; /* negative sign */
     ret = malloc(length + 1); /* create new string */
     if (!ret)
-        return (NULL);
+  return (NULL);
 
 
     fill_numbase_buff(temp, 10, ret, length);
     if (num < 0 || num_l < 0)
-        ret[0] = '-';
+      ret[0] = '-';
 
 
     return (ret);
